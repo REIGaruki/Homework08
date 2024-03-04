b6e6b6a5d5b69eea5f29d7aaae545942a643ec48
REIGaruki <gyakujo@mail.ru>
2024-03-04 08:42:27 +0300

    Task1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Массивы");
+        int [] task = {1, 2, 3, 4};
+        System.out.println("Задание " + task[0]);
+        int [] intArray = new int[];
+        for (int i = 0; i < intArray.length; i++) {
+            intArray[i] = i + 1;
+        }
+        double [] floatArray = {1.57, 7.654, 9.986};
+        double [] newArray = new double[]{1.57, 7.654, 9.986};
+
     }
 }
