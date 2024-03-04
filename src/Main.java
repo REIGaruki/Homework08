c17323ab458648253ab0e5bec413a695ddbad3b1
REIGaruki <gyakujo@mail.ru>
2024-03-04 08:50:00 +0300

    Task2

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,32 @@ public class Main {
         System.out.println("Массивы");
         int [] task = {1, 2, 3, 4};
         System.out.println("Задание " + task[0]);
-        int [] intArray = new int[];
+        int [] intArray = new int[3];
         for (int i = 0; i < intArray.length; i++) {
             intArray[i] = i + 1;
         }
         double [] floatArray = {1.57, 7.654, 9.986};
-        double [] newArray = new double[]{1.57, 7.654, 9.986};
-
+        double [] newArray = new double[]{12.345, 0.000_000_001, 9.99, 875.497};
+        System.out.println("Задание " + task[1]);
+        for (int i = 0; i < intArray.length; i++) {
+            System.out.print(intArray[i]);
+            if (i < intArray.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+        for (int i = 0; i < floatArray.length; i++) {
+            System.out.print(floatArray[i]);
+            if (i < floatArray.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+        for (int i = 0; i < newArray.length; i++) {
+            System.out.print(newArray[i]);
+            if (i < newArray.length - 1) {
+                System.out.print(", ");
+            }
+        }
     }
 }
